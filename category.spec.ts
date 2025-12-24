0a658c437b01fb3b9646e4ef4830abae007ace3e
rahafzaw <151791880+rahafzaw@users.noreply.github.com>
2025-12-24 23:45:19 +0200

    Update category.spec.ts

--- category.spec.ts
+++ category.spec.ts
@@ -1,14 +1,1 @@
-import { test } from '@playwright/test';
-import { ProductsPage } from '../../pages/ProductsPage';
 
-test.use({ storageState: 'auth.json' });
-
-test.describe('Filter by Category', () => {
-  test('User can filter products by category', async ({ page }) => {
-    const productsPage = new ProductsPage(page);
-
-    await productsPage.goto();
-    await productsPage.filterByCategory('Hammer');
-    await productsPage.assertHasResults();
-  });
-});
